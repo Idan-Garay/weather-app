fe22e1785360adb9e30671e084761ff72f7ba1e0
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-06-22 02:54:24 +0800

    Add temperature

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import { fetchWeather } from '@/actions'
 import { WeatherForm } from '@/components/WeatherForm'
 
-export default function Home() {
+export default async function Home() {
+  const weather = await fetchWeather('New York City')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
       <div className="bg-image-background blur-sm absolute z-0 inset-0"></div>
       <div className="z-10">
-        <WeatherForm />
+        <WeatherForm defaultWeather={weather} />
       </div>
     </main>
   )
--- components/WeatherForm/index.tsx
+++ components/WeatherForm/index.tsx
@@ -11,11 +11,17 @@ export const WeatherFormSchema = z.object({
   city: z.string().min(1),
 })
 
-export const WeatherForm = () => {
+export const WeatherForm = ({
+  defaultWeather,
+}: {
+  defaultWeather: Weather | undefined
+}) => {
   const [city, setCity] = useState('New York City')
-  const [weather, setWeather] = useState<Weather>()
+  const [weather, setWeather] = useState(defaultWeather)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
     event.preventDefault()
     const formState = WeatherFormSchema.safeParse({ city })
     if (formState.success) {
@@ -24,29 +30,39 @@ export const WeatherForm = () => {
     } else {
       alert('City input has at least 1 character')
     }
+    setIsLoading(false)
   }
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex items-center gap-3 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+      className=" gap-3 absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
     >
-      <Input
-        type="text"
-        name="city"
-        placeholder="City"
-        className="text-white bg-transparent border-transparent border-b-white border rounded-none outline-none  text-3xl text-center"
-        onChange={setCity}
-        value={city}
-      />
-      <button type="submit" className="group">
-        <Image
-          src="/img/partly-cloudy.png"
-          alt="partly cloudy"
-          height={64}
-          width={64}
-          className=" group-hover:-translate-y-1 transition duration-300"
+      <div className="flex items-center">
+        <Input
+          type="text"
+          name="city"
+          placeholder="City"
+          className="text-white bg-transparent border-transparent border-b-white border rounded-none outline-none  text-3xl text-center"
+          onChange={setCity}
+          value={city}
         />
-      </button>
+        <button type="submit" className="group">
+          <Image
+            src="/img/partly-cloudy.png"
+            alt="partly cloudy"
+            height={64}
+            width={64}
+            className=" group-hover:-translate-y-1 transition duration-300"
+          />
+        </button>
+      </div>
+      <div
+        className={`text-3xl text-center ${
+          isLoading ? 'blur-sm' : ''
+        } transition`}
+      >
+        {weather?.main.temp} {'℉'}
+      </div>
     </form>
   )
 }
