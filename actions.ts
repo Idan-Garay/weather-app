41bfb5cd5bec842e65cbcf745eb9f4b957f0b89c
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2024-06-22 02:59:03 +0800

    refetch on every request

--- actions.ts
+++ actions.ts
@@ -5,7 +5,8 @@ import { Weather } from './types/weather'
 export const fetchWeather = async (city = 'New York City') => {
   try {
     const res = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=246e9e4aa5bd8ca3506e9af768b84812&units=imperial`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=246e9e4aa5bd8ca3506e9af768b84812&units=imperial`,
+      { cache: 'no-store' }
     )
 
     const data = (await res.json()) as Weather
